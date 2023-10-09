65b0d732082f5e5bbe95cd0468a1a2c3e059335d
MateoGuerreroE <mateof1223@gmail.com>
2023-10-08 20:04:07 -0500

    Update 8/10

--- src/components/Landing/Landing.jsx
+++ src/components/Landing/Landing.jsx
@@ -1,9 +1,18 @@
 import StyledLanding from "./Landing.module.css";
+import mainDog from "../../content/landing_orangeDog.svg";
+import { NavLink } from "react-router-dom";
 
 export default function Landing() {
   return (
-    <div>
-      <p>Landing</p>
+    <div className={StyledLanding.main}>
+      <img
+        className={StyledLanding.mainDog}
+        src={mainDog}
+        alt="Main Orange Dog"
+      />
+      <button>
+        <NavLink to="/home">Go to Home</NavLink>
+      </button>
     </div>
   );
 }
