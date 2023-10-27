663fd8205a87b999a199059e58c702fa7f74577f
MateoGuerreroE <mateof1223@gmail.com>
2023-10-27 09:40:39 -0500

    SOme responsivity fixed

--- src/components/SearchBar/SearchBar.jsx
+++ src/components/SearchBar/SearchBar.jsx
@@ -14,17 +14,19 @@ export default function SearchBar() {
 
   // HANDLERS
 
-  function handleClick() {
-    const { value } = document.getElementById("S_input");
-    if (value) {
-      dispatch(searchDog(value));
-      document.getElementsByName("origin")[0].value = "All";
-      document.getElementsByName("temperaments")[0].value = "All";
-      sessionStorage.setItem("temperaments", "All");
-      sessionStorage.setItem("origin", "All");
-      document.getElementById("S_input").value = "";
-    } else {
-      alert("Introduce una raza para poder buscarla");
+  function handleClick(event) {
+    if (event.type === "click" || event.code === "Enter") {
+      const { value } = document.getElementById("S_input");
+      if (value) {
+        dispatch(searchDog(value));
+        document.getElementsByName("origin")[0].value = "All";
+        document.getElementsByName("temperaments")[0].value = "All";
+        sessionStorage.setItem("temperaments", "All");
+        sessionStorage.setItem("origin", "All");
+        document.getElementById("S_input").value = ""; //! Added
+      } else {
+        alert("Introduce a breed to search");
+      }
     }
   }
 
